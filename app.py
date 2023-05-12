109e624cac2029e18de46399dc4554361e3fad97
Beth Carretta <bethcarretta@gmail.com>
2023-05-12 15:01:24 -0400

    added POST /api/cupcakes route

--- app.py
+++ app.py
@@ -15,17 +15,31 @@ toolbar = DebugToolbarExtension(app)
 connect_db(app)
 app.app_context().push()
 
-@app.route('/api/cupcakes')
+@app.route("/api/cupcakes")
 def list_cupcakes():
     """Returns json of all cupcakes resources"""
     all_cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
     return jsonify(cupcakes=all_cupcakes)
 
-@app.route('/api/cupcakes/<int:id>')
+@app.route("/api/cupcakes/<int:id>")
 def get_cupcake(id):
     """Returns json of a single cupcake resource"""
     cupcake = Cupcake.query.get_or_404(id)
     return jsonify(cupcake=cupcake.serialize())
+
+@app.route("/api/cupcakes", methods=["POST"])
+def create_cupcake():
+    """Add a new cupcake resource to db"""
+    new_cupcake = Cupcake(
+        flavor=request.json["flavor"],
+        size=request.json["size"],
+        rating=request.json["rating"],
+        image=request.json["image"]
+    )
+    db.session.add(new_cupcake)
+    db.session.commit()
+    response_json = jsonify(cupcake=new_cupcake.serialize())
+    return (response_json, 201)
 
 if __name__ == "__main__":
     app.run(debug=True)
