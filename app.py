f94912d6a3e8555282bedeb54ec4c40f3ac79530
Beth Carretta <bethcarretta@gmail.com>
2023-05-13 10:30:16 -0400

    added templates for index/new cupcake form, added /cupcakes/new route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect, flash
 from models import db, connect_db, Cupcake
 from flask_debugtoolbar import DebugToolbarExtension
 import os
@@ -18,6 +18,32 @@ toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 app.app_context().push()
+
+@app.route('/')
+def show_cupcakes():
+    """Renders html template with a list of all cupcakes"""
+    cupcakes = Cupcake.query.all()
+    return render_template('index.html', cupcakes=cupcakes)
+
+@app.route('/cupcakes/new')
+def show_cupcake_form():
+    """Shows form to create new cupcake"""   
+    return render_template('new_cupcake_form.html')
+
+@app.route('/cupcakes/new', methods=["POST"])
+def handle_cupcake_form():
+    """Adds new cupcake to db"""  
+    flavor = request.form.get('flavor')
+    size = request.form.get('size')
+    image = request.form.get('image', None)
+    rating= request.form.get('rating')
+
+    cupcake = Cupcake(flavor=flavor, size=size, image=image, rating=rating)
+    db.session.add(cupcake)
+    db.session.commit()
+
+    flash(f"A new {cupcake.flavor} cupcake was added!")
+    return redirect('/')
 
 @app.route("/api/cupcakes")
 def list_cupcakes():
